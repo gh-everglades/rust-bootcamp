eff8df1b9e5931424a9b8c4754ec9eb575e6e22b
gh <guilherme.heinen@everglades.tech>
2024-09-11 14:01:24 +0200

    Completed Step 1/Stage 2

--- 4. Projects/2. API-axum/Problem/Stage 2/Step 1/src/main.rs
+++ 4. Projects/2. API-axum/Problem/Stage 2/Step 1/src/main.rs
@@ -1,4 +1,8 @@
 // TODO: import log, pretty_env_logger, dotenv, and PgPoolOptions
+use log::info;
+use pretty_env_logger;
+use std::env;
+use sqlx::postgres::PgPoolOptions;
 
 use axum::{
     routing::{delete, get, post},
@@ -13,7 +17,11 @@ use handlers::*;
 #[tokio::main]
 async fn main() {
     // TODO: Initialize pretty_env_logger
+    pretty_env_logger::init();
+
     // TODO: Initialize dotenv
+    dotenvy::dotenv();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
     // Create a new PgPoolOptions instance with a maximum of 5 connections.
     // Use dotenv to get the database url.
@@ -21,14 +29,21 @@ async fn main() {
     // error occurs at this stage the server should be terminated.
     // See examples on GitHub page: https://github.com/launchbadge/sqlx
     // let pool = todo!();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(database_url.as_str()).await.unwrap();
 
     // Using slqx, execute a SQL query that selects all questions from the questions table.
     // Use the `unwrap` or `expect` method to handle errors. This is just some test code to
     // make sure we can connect to the database.
     // let recs = todo!();
+    let recs =  sqlx::query!("SELECT * FROM questions")
+                .fetch_all(&pool)
+                .await.unwrap();
 
     info!("********* Question Records *********");
     // TODO: Log recs with debug formatting using the info! macro
+    info!("{:?}", recs);
 
     let app = Router::new()
         .route("/question", post(create_question))
