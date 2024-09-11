0ad692dd1c8ac1121654bc73b9908bb5b252b221
gh <guilherme.heinen@everglades.tech>
2024-09-11 12:22:24 +0200

    API project: Stage 1 complete

--- 4. Projects/2. API-axum/Problem/Stage 1/Step 1 & 2/src/handlers/mod.rs
+++ 4. Projects/2. API-axum/Problem/Stage 1/Step 1 & 2/src/handlers/mod.rs
@@ -1,18 +1,40 @@
 use crate::models::*;
-use axum::{response::IntoResponse, Json};
+use axum::{http::StatusCode, response::IntoResponse, Json};
 
 // ---- CRUD for Questions ----
 
 pub async fn create_question(Json(question): Json<Question>) -> impl IntoResponse {
-    todo!()
+    // TODO: Create a POST route to /question which accepts a `Question` and returns `QuestionDetail` as JSON.
+    (StatusCode::OK, 
+        Json(
+            QuestionDetail {
+                question_uuid: String::from("d347261c-3f0e-42d2-8706-5ef9f1b96725"),
+                title: question.title.clone(),
+                description: question.description.clone(),
+                created_at: String::from("2024-09-11T12:00:00Z"),
+            }
+        )
+    )
+
 }
 
 pub async fn read_questions() -> impl IntoResponse {
-    todo!()
+    (StatusCode::OK, 
+        Json(
+            [
+            QuestionDetail {
+                question_uuid: String::from("d347261c-3f0e-42d2-8706-5ef9f1b96725"),
+                title: String::from("Newly Created Question"),
+                description: String::from("My Description"),
+                created_at: String::from("2024-09-11T12:00:00Z"),
+            }
+            ]
+        )
+    )
 }
 
-pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
-    todo!()
+pub async fn delete_question(Json(question_uuid): Json<QuestionId>) -> impl IntoResponse {
+   StatusCode::OK
 }
 
 // ---- CRUD for Answers ----
@@ -22,11 +44,40 @@ pub async fn delete_question(Json(question_uuid): Json<QuestionId>) {
 //
 //       hint: this function should look very similar to the create_question function above
 
+pub async fn create_answer(Json(answer): Json<Answer>) -> impl IntoResponse {
+    (StatusCode::OK, 
+        Json(
+            AnswerDetail {
+                answer_uuid: String::from("43217890-a685-4695-9784-277246b16567"),
+                question_uuid: answer.question_uuid.clone(),
+                content: answer.content.clone(),
+                created_at: String::from("2024-09-11T12:00:00Z"),
+            }
+        )
+    )
+}
 // TODO: Create a GET route to /answers which accepts an `QuestionId` and returns a vector of `AnswerDetail` as JSON.
 //       The handler function should be called `read_answers`.
 //
 //       hint: this function should look very similar to the read_questions function above
+pub async fn read_answers(Json(question_id): Json<QuestionId>) -> impl IntoResponse {
+    (StatusCode::OK, 
+        Json(
+            [
+            AnswerDetail {
+                answer_uuid: String::from("43217890-a685-4695-9784-277246b16567"),
+                question_uuid: question_id.question_uuid.clone(),
+                content: String::from("My Answer"),
+                created_at: String::from("2024-09-11T12:00:00Z"),
+            }
+            ]
+        )
+    )
+}
 
+pub async fn delete_answer(Json(answer_uuid): Json<AnswerId>) -> impl IntoResponse {
+    StatusCode::OK
+}
 // TODO: Create a DELETE route to /answer which accepts an `AnswerId` and does not return anything.
 //       The handler function should be called `delete_answer`.
 //
